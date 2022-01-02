106ebe4e4838c434a689aa5b971c31fc18581788
chuefengv <feng.vang98@gmail.com>
2022-01-01 20:02:03 -0500

    centered the sizzle for opening menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import sizzle from './assets/sizzle.mp4'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app-wrapper">
+      <div className='video-wrapper'>
+        <video className='videoTag' autoPlay loop muted>
+            <source src={sizzle} type='video/mp4' />
+        </video>
+      </div>
+      hello
     </div>
   );
 }
